79a2d6b0faaf18347be34326a51835ded80c8b53
duc <duc2820@gmail.com>
2018-10-14 07:25:07 +0700

    start make game level 1

---
+++ gameBoune.js
@@ -0,0 +1,25 @@
+class Boune {
+    constructor(x, y , R ){
+
+    }
+}
+class Column {
+    constructor(d , r) {
+
+    }
+}
+class BackgroundGame {
+    constructor( canvas  , width, height  , colorBackground ){
+
+    }
+}
+class ControlGame {
+        constructor(canvas ){
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+            canvas.colorBackground = 'blue'
+        }
+        
+}
+
+const canvas = document.getElementById('canvas')
